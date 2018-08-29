0a34abccceac6bfe971f3e386c063515fcfcba76
David Heyman <dave@axismaps.com>
2018-08-29 16:06:12 +0100

    Found a few missing files

--- build.sh
+++ build.sh
@@ -4,8 +4,10 @@ cp node_modules/jquery-range/jquery.range.css public/jquery.range.css
 cp semantic/dist/semantic.min.css public/semantic.min.css
 
 cp node_modules/mapbox-gl/dist/mapbox-gl.js public/mapbox-gl.js
+cp node_modules/mapbox-gl/dist/mapbox-gl.js.map public/mapbox-gl.js.map
 cp node_modules/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.min.js public/mapbox-gl-geocoder.min.js
 cp node_modules/jquery/dist/jquery.min.js public/jquery.min.js
 cp node_modules/underscore/underscore-min.js public/underscore-min.js
 cp node_modules/jquery-range/jquery.range-min.js public/jquery.range-min.js
 cp semantic/dist/semantic.min.js public/semantic.min.js
+cp -r semantic/dist/themes/ public/themes
