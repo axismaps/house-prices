646211c3bce93f878f8ec147efa2b3500508cc2d
David Heyman <dave@axismaps.com>
2018-08-29 16:00:14 +0100

    Deploy and build

---
+++ build.sh
@@ -0,0 +1,11 @@
+cp node_modules/mapbox-gl/dist/mapbox-gl.css public/mapbox-gl.css
+cp node_modules/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css public/mapbox-gl-geocoder.css
+cp node_modules/jquery-range/jquery.range.css public/jquery.range.css
+cp semantic/dist/semantic.min.css public/semantic.min.css
+
+cp node_modules/mapbox-gl/dist/mapbox-gl.js public/mapbox-gl.js
+cp node_modules/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.min.js public/mapbox-gl-geocoder.min.js
+cp node_modules/jquery/dist/jquery.min.js public/jquery.min.js
+cp node_modules/underscore/underscore-min.js public/underscore-min.js
+cp node_modules/jquery-range/jquery.range-min.js public/jquery.range-min.js
+cp semantic/dist/semantic.min.js public/semantic.min.js
